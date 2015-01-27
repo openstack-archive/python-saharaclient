28a5c6f7a1556e20b6b5a2bcc74a21c7d23fdf58
Andrey Pavlov <apavlov@mirantis.com>
2015-01-27 10:24:57 +0300

    Using oslo_* instead of oslo.*
    Closes-bug: #1414587
    
    Change-Id: I22b743b6e63bd847d5cc11b83229aa9810eaae38

--- saharaclient/client.py
+++ saharaclient/client.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from oslo.utils import importutils
+from oslo_utils import importutils
 
 
 class UnsupportedVersion(Exception):
