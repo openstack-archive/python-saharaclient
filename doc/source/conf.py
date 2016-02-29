409c48bf1f331ed3e4d1f3388568a3a0e19917da
Andreas Jaeger <aj@suse.com>
2016-02-29 19:58:37 +0100

    Remove unused pngmath Sphinx extension
    There's no RST file that uses ".. math" and thus
    the pngmath Sphinx extension is not used and can
    get removed.
    
    Change-Id: I3ea4b529025c8ac3c4092f3720124647861e6668

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -38,7 +38,7 @@ sys.path.append(os.path.abspath('../bin'))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo',
-              'sphinx.ext.coverage', 'sphinx.ext.pngmath',
+              'sphinx.ext.coverage',
               'sphinx.ext.viewcode', 'ext.cli']
 
 if not on_rtd:
