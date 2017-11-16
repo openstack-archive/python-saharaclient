d9839c1fb7376cc3f859284e1158b377fd1cd23e
Andreas Jaeger <aj@suse.com>
2017-11-16 20:48:13 +0100

    Remove setting of version/release from releasenotes
    Release notes are version independent, so remove version/release
    values. We've found that projects now require the service package
    to be installed in order to build release notes, and this is entirely
    due to the current convention of pulling in the version information.
    
    Release notes should not need installation in order to build, so this
    unnecessary version setting needs to be removed.
    
    This is needed for new release notes publishing, see
    I56909152975f731a9d2c21b2825b972195e48ee8 and the discussion starting
    at
    http://lists.openstack.org/pipermail/openstack-dev/2017-November/124480.html
    .
    
    Change-Id: Idb0808b17a34a9c121a922eaa9317d30642a0736

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -38,16 +38,11 @@ master_doc = 'index'
 project = u'Saharaclient Release Notes'
 copyright = u'2015, Sahara Developers'
 
-# The version info for the project you're documenting, acts as replacement for
-# |version| and |release|, also used in various other places throughout the
-# built documents.
-#
-# The short X.Y version.
-from saharaclient.version import version_info as saharaclient_version
+# Release notes are version independent.
 # The full version, including alpha/beta/rc tags.
-release = saharaclient_version.version_string_with_vcs()
+release = ''
 # The short X.Y version.
-version = saharaclient_version.canonical_version_string()
+version = ''
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
